92c26a21897770391b5ed7b6c0b9454080a10192
hsaeed3 <hsaeed3@student.ohlone.edu>
2025-02-14 03:09:14 -0800

    refactor: remove 'state' from root init

--- chatspec/__init__.py
+++ chatspec/__init__.py
@@ -1,6 +1,9 @@
 """
-💭 chatspec
+## 💭 chatspec
 
 Types & utilities built for processing & interacting with objects used in
 the chat completions API specification.
 """
+
+from .utils import *
+from . import types
