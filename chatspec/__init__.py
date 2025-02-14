42ec09dd9420b85b21cc6eff0c05471f3417cf8f
hsaeed3 <hsaeed3@student.ohlone.edu>
2025-02-14 13:26:02 -0800

    feat(chatspec): build chatspec.utils as chatspec._main; add annotations

--- chatspec/__init__.py
+++ chatspec/__init__.py
@@ -5,6 +5,5 @@ Types & utilities built for processing & interacting with objects used in
 the chat completions API specification.
 """
 
-from .utils import *
-from .state import State
-from . import types
+from ._main import *
+from . import params, types
