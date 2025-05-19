6d828da5c253c07bbb637f47bb15aecbabb72df4
hsaeed3 <hsaeed3@student.ohlone.edu>
2025-05-19 15:36:56 -0700

    refactor(package): reorganize package structure and enhance README to reflect recent changes and features

---
+++ prompted/types/__init__.py
@@ -0,0 +1,7 @@
+"""
+💭 prompted.types
+
+Contains type definitions, models and light utilities for types 
+found within the `Chat Completions`, `Anthropic Model Context Protocol`
+and `Google A2A` specifications.
+"""
