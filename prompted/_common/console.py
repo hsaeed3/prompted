f24dbf687af4b2b7d15bd0e77ec1fee7e26155aa
hsaeed3 <hsaeed3@student.ohlone.edu>
2025-06-01 19:21:35 -0700

    refactor(prompted): consolidate logging functionality into console module; remove logger module and enhance logging setup with rich formatting

--- prompted/_common/console.py
+++ prompted/_common/console.py
@@ -1,9 +1,13 @@
 """✼ prompted._common.console"""
 
+import logging
 from typing import Literal, TypeAlias
 from rich.console import Console
+from rich.logging import RichHandler
+
 
 __console__: None | Console = None
+"""Singleton console instance for the `prompted` package."""
 
 
 RichColor: TypeAlias = Literal[
@@ -232,4 +236,67 @@ def set_console(console: Console) -> None:
     __console__ = console
 
 
-__all__ = ["get_console", "set_console", "RichColor"]
+class RichMarkupFilter(logging.Filter):
+    def filter(self, record):
+        if record.levelno >= logging.CRITICAL:
+            record.msg = f"[bold red]{record.msg}[/bold red]"
+        elif record.levelno >= logging.ERROR:
+            record.msg = f"[italic red]{record.msg}[/italic red]"
+        elif record.levelno >= logging.WARNING:
+            record.msg = f"[italic yellow]{record.msg}[/italic yellow]"
+        elif record.levelno >= logging.INFO:
+            record.msg = f"[white]{record.msg}[/white]"
+        elif record.levelno >= logging.DEBUG:
+            record.msg = f"[italic dim]{record.msg}[/italic dim]"
+        return True
+
+
+def _setup_logging() -> logging.Logger:
+    logger = logging.getLogger("prompted")
+
+    handler = RichHandler(
+        level=logging.WARNING,
+        console=get_console(),
+        rich_tracebacks=True,
+        show_time=False,
+        show_path=False,
+        markup=True,
+    )
+    formatter = logging.Formatter("| [bold]{name}[/bold] - {message}", style="{")
+    handler.setFormatter(formatter)
+    handler.addFilter(RichMarkupFilter())
+    if logger.hasHandlers():
+        logger.handlers.clear()
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+    return logger
+
+
+def _get_logger(module: str | None = None) -> logging.Logger:
+    if module is None:
+        return _logger
+    return _logger.getChild(module)  # type: ignore
+
+
+def verbosity(
+    level: Literal["debug", "info", "warning", "error", "critical"],
+) -> None:
+    logger = _get_logger()
+    logger.setLevel(level.upper())
+    # Update all handlers' levels to match
+    for handler in logger.handlers:
+        handler.setLevel(level.upper())
+
+
+_logger = _setup_logging()
+"""Singleton logger for the `prompted` package."""
+
+
+__all__ = [
+    "get_console",
+    "set_console",
+    "RichColor",
+    "verbosity",
+    "_get_logger",
+]
