0f8a29537ff92d96f322605492478bf2789405e4
hsaeed3 <hammadaidev@gmail.com>
2025-06-09 19:59:46 -0700

    feat(schema): introduce prompted.schema module to define API specification formats for Generative AI operations

---
+++ prompted/schema/__init__.py
@@ -0,0 +1,9 @@
+"""prompted.schema
+
+Contains various resources and types that are used to represent
+various API specification formats for completions, using language
+models, and other Generative AI operations.
+
+Currently only the `openai` schema has been added to the `prompted`
+framework.
+"""
