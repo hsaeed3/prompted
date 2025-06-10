ea857d7eed53ec4e9dc462de9ab2470f36fd18b9
hsaeed3 <hammadaidev@gmail.com>
2025-06-09 19:37:24 -0700

    delete(openai): remove OpenAI specifications and type definitions for embeddings, messages, responses, and tools to streamline the package

--- prompted/specifications/__init__.py
+++
@@ -1,7 +0,0 @@
-"""prompted.specifications
-
-Contains various types that define the specification for an
-API provider for language models.
-
-Currently only the `openai` specification has been added to
-the `prompted` framework."""
--- prompted/specifications/openai/__init__.py
+++
@@ -1,5 +0,0 @@
-"""prompted.specifications.openai
-
-Contains various models, type definitions and resources
-that are specific to using the OpenAI Chat Completions
-API specification with the objects within `prompted`."""
