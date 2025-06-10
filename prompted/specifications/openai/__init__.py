cb00b01f58693289ffa72263ee669df8c00e6fbf
Hammad Saeed <hsaeed@csod.com>
2025-06-09 23:21:28 -0700

    delete(specifications): remove OpenAI specifications and types to streamline the package

--- prompted/specifications/openai/__init__.py
+++
@@ -1,4 +0,0 @@
-"""prompted.specifications.openai
-
-Contains resources and types that are used to define objects and convert
-to and from models in the OpenAI Chat Completions API specification."""
