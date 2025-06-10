b4461c120f99b2ad138c90a9144e6dc5625a0f84
hsaeed3 <hammadaidev@gmail.com>
2025-06-09 19:11:48 -0700

    feat(openai): add OpenAI specifications and tools for API integration, including type definitions and function models

---
+++ prompted/specifications/openai/__init__.py
@@ -0,0 +1,5 @@
+"""prompted.specifications.openai
+
+Contains various models, type definitions and resources
+that are specific to using the OpenAI Chat Completions
+API specification with the objects within `prompted`."""
