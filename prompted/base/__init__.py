4b9d627ef33d72d323640eb52d1a5f0a15c0a351
hsaeed3 <hammadaidev@gmail.com>
2025-06-10 02:43:46 -0700

    delete(base): remove prompted.base module and associated Specification class to streamline the package

--- prompted/base/__init__.py
+++
@@ -1,5 +0,0 @@
-"""prompted.base
-
-Contains base objects that define the interface for and are used throughout
-the `prompted` framework. None of these objects are meant to be used directly
-and are used rather for internal type hinting and implenation"""
